e8b250f3fb5d84b8b7a59c6180d713ae69da947c
Abhilash M <abhilashmknr143@gmail.com>
2026-07-09 21:04:24 +0530

    style: apply dart formatting to fix CI

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -17,7 +17,9 @@ class NotificationService {
     tz.initializeTimeZones();
 
     // Android Settings: Using default launcher icon
-    const androidSettings = AndroidInitializationSettings('@mipmap/launcher_icon');
+    const androidSettings = AndroidInitializationSettings(
+      '@mipmap/launcher_icon',
+    );
 
     // iOS Settings
     const iosSettings = DarwinInitializationSettings(
