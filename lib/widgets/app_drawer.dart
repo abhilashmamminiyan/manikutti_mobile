25d50dac747373a7cd2c24e36f4a53bf5da3e9a7
Abhilash M <abhilashmknr143@gmail.com>
2026-07-10 00:13:43 +0530

    Fix localization imports and CI build number

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import '../l10n/app_localizations.dart';
 import '../screens/dashboard_screen.dart';
 import '../screens/settings_screen.dart';
 import '../screens/smart_scan_screen.dart';
